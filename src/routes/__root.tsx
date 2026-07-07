0d9d44684694f808312b8c46ce7829e63ddb679f
vishurajputt07 <vishurajput361@gmail.com>
2026-07-07 21:14:39 +0530

    updated page logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -95,7 +95,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon.png", type: "image/x-icon" },
       { rel: "preconnect", href: "https://api.fontshare.com" },
       { rel: "preconnect", href: "https://cdn.fontshare.com", crossOrigin: "anonymous" },
       {
